74bc07bafcca9013bcb1d8e34f8ac7c96273b087
Zakisee <1210143978@qq.com>
2022-07-21 20:02:30 +0800

    fix: 完成数值练习

--- learning/4运算操作/string.ts
+++ learning/4运算操作/string.ts
@@ -4,4 +4,11 @@ type StrLen<Str extends string, Arr extends unknown[] = []> =
     Str extends `${string}${infer Rest}` ? StrLen<Rest, [...Arr,unknown]> : Arr['length']
     
 
-type StrLenRes = StrLen<'zhangjing'>
+type StrLenRes = StrLen<'222dsdsd'>
+
+
+//------------------------------
+// 两个数的比较
+type GreateThan<Num1 extends number, Num2 extends number, Results extends unknown[] = []> =
+    Num1 extends Num2 ? false : Results['length'] extends Num2 ? true : Results['length'] extends Num1 ? false : GreateThan<Num1, Num2, [...Results, unknown]>
+type GreateThanRes = GreateThan<1,2>
